1b63d14433d9684288392668e37ee22744e2e868
Nick Mosher <nicholastmosher@gmail.com>
2018-02-02 10:31:19 -0500

    Add boilerplate crates

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,3 +1,45 @@
+#[macro_use] extern crate log;
+extern crate failure;
+extern crate clap;
+extern crate simplelog;
+extern crate emjc_lib as emjc;
+
+use failure::Error;
+
+use clap::{
+    App,
+    AppSettings,
+    Arg,
+    ArgMatches,
+};
+
+use simplelog::{
+    SimpleLogger,
+    LevelFilter,
+    Config,
+};
+
+fn app<'a, 'b>() -> App<'a, 'b> {
+    App::new("emjc")
+        .settings(&[
+            AppSettings::ColoredHelp,
+        ])
+        .arg(Arg::with_name("blah"))
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _ = SimpleLogger::init(LevelFilter::Debug, Config::default());
+    debug!("Initialized logger");
+
+    let matches = app().get_matches();
+    match execute(&matches) {
+        Ok(_) => return,
+        Err(e) => println!("{}", e),
+    }
+}
+
+fn execute(_: &ArgMatches) -> Result<(), Error> {
+
+    emjc::lex();
+    Ok(())
 }
---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+#[macro_use] extern crate log;
+extern crate regex;
+
+pub fn lex() {
+    debug!("Started lexing");
+}
